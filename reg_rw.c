3429b448544fcc5bf525e21095a2bcf0ecc3e426
Haruki NAKAI <nakai.haruki.gb@lila.cs.tsukuba.ac.jp>
2021-09-27 16:21:04 +0900

    reg_rw.c : 不要なコードを削除、エラーメッセージ追加

--- reg_rw.c
+++ reg_rw.c
@@ -18,12 +18,8 @@
 #include <byteswap.h>
 #include <string.h>
 #include <errno.h>
-#include <signal.h>
 #include <fcntl.h>
-#include <ctype.h>
-#include <termios.h>
 
-#include <sys/types.h>
 #include <sys/mman.h>
 
 /* ltoh: little to host */
@@ -48,18 +44,13 @@
 #define MAP_SIZE (1*1024*1024UL)
 #define MAP_MASK (MAP_SIZE - 1)
 
-typedef enum {
-	MODE_READ,
-	MODE_WRITE
-} rwmode;
-
 void* map_base;
 
 uint32_t read_from_device(off_t target_addr){
-	/* calculate the virtual address to be accessed */
+	/* 仮想アドレスを計算 */
 	void* virt_addr = map_base + target_addr;
 	uint32_t read_result = *((uint32_t *) virt_addr);
-	/* swap 32-bit endianess if host is not little-endian */
+	/* ホストがリトルエンディアンでないならば、エンディアンを反転する */
 	read_result = ltohl(read_result);
 #ifdef DEBUG
 	printf("Read 32-bit value at address 0x%08x (%p): 0x%08x\n",
@@ -71,14 +62,14 @@ uint32_t read_from_device(off_t target_addr){
 }
 
 void write_to_device(off_t target_addr, uint32_t writeval){
-	/* calculate the virtual address to be accessed */
+	/* 仮想アドレスを計算 */
 	void* virt_addr = map_base + target_addr;
 #ifdef DEBUG
 	printf("Write 32-bits value 0x%08x to 0x%08x (0x%p)\n",
 			(unsigned int)writeval, (unsigned int)target_addr,
 			virt_addr);
 #endif
-	/* swap 32-bit endianess if host is not little-endian */
+	/* ホストがリトルエンディアンでないならば、エンディアンを反転する */
 	writeval = htoll(writeval);
 	*((uint32_t *) virt_addr) = writeval;
 	fflush(stdout);
@@ -87,9 +78,6 @@ void write_to_device(off_t target_addr, uint32_t writeval){
 int main(int argc, char **argv)
 {
 	int fd;
-	uint32_t read_result;
-	uint32_t writeval = 0x00000000;
-	off_t target_addr = 0x0000002c;
 	char *device = "/dev/xdma0_user"; // xdma*_user : DMA/Bridge IPのaxi4-liteマスターに対応するデバイスノード
 #ifdef DEBUG
 	printf("device: %s\n", device);
@@ -97,16 +85,18 @@ int main(int argc, char **argv)
 #endif
 
 	if ((fd = open(device, O_RDWR | O_SYNC)) == -1) {
+		fprintf(stderr, "Character Device \"%s\" cannot opened.\n", device);
 		FATAL;
 	}
 #ifdef DEBUG
-	printf("character device %s opened.\n", device);
+	printf("Character Device \"%s\" opened.\n", device);
 	fflush(stdout);
 #endif
 
 	/* map one page */
 	map_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if (map_base == (void *)-1) {
+		fprintf(stderr, "mmap() failed.\n");
 		FATAL;
 	}
 #ifdef DEBUG
@@ -114,13 +104,15 @@ int main(int argc, char **argv)
 	fflush(stdout);
 #endif
 
+	off_t target_addr = 0x0000002c;
 	uint32_t counter1 = read_from_device(target_addr);
 	uint32_t counter2 = read_from_device(target_addr);
 	int counter_diff = counter2 - counter1;
 
-	printf("Counter diff:%d = %.2f ns\n", counter_diff, (double)counter_diff/250);
+	printf("Counter diff:%d = %.2f ns\n", counter_diff, (double)counter_diff/250); //クロックが250MHzならばこう
 
 	if (munmap(map_base, MAP_SIZE) == -1) {
+		fprintf(stderr, "munmap() failed.\n");
 		FATAL;
 	}
 	close(fd);
